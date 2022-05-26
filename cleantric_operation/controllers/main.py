dcacf6a0360bed5b9af019435554c1ecef08cc06
ptsuva <ptsuva@ptsuva-Inspiron-5402>
2022-05-26 20:51:38 +0530

    [UPD] Update on Shop/Package page. (portal user must have to login)

--- cleantric_operation/controllers/main.py
+++ cleantric_operation/controllers/main.py
@@ -1,6 +1,9 @@
 from odoo import http, _
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import get_records_pager, CustomerPortal
+from odoo.addons.website.models.ir_http import sitemap_qs2dom
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
     
 class CustomerPortal(CustomerPortal):
@@ -24,9 +27,23 @@ class CustomerPortal(CustomerPortal):
         return res
 
 
+class WebsiteSale(WebsiteSale):
+    """
+            this controller inherit shop page and the user not login then force fully
+             redirect login page.
+    """
+    @http.route([
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category"):category>''',
+        '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
+    ], type='http', auth="public", website=True, sitemap=WebsiteSale.sitemap_shop)
+    def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
 
-
-
+        if not request.session.uid:
+            return request.redirect("/web/login?redirect=/shop")
+        res = super(WebsiteSale, self).shop(page=page, category=category, search=search,min_price=min_price, max_price=max_price, ppg=ppg, **post)
+        return res
 
 
 

5ce13b355035c74f61c6b4fdcf20c216ed9cebc2
dominique <dominique.k@elico-corp.com.sg>
2022-06-16 11:30:42 +0800

    fix init.py

--- cleantric_operation/__manifest__.py
+++ cleantric_operation/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Alitec Pte Ltd",
     'website': "http://www.alitec.sg",
     'version': '0.1.0.0.5',
-    'depends': ['crm', 'website_crm', 'website', 'business_appointment_website'],
+    'depends': ['crm', 'website_crm', 'website', 'business_appointment_website', 'portal'],
     'data': [
         'views/contact_us.xml',
         'views/appointment_checkout_form.xml',

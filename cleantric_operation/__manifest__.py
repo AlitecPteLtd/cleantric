86f6c409d6a1c4375a793ecfd9540e8d9fd539bf
ptsuva <ptsuva@ptsuva-Inspiron-5402>
2022-06-23 15:26:13 +0530

    [UPD] Update Appintment Address readonly name & email

--- cleantric_operation/__manifest__.py
+++ cleantric_operation/__manifest__.py
@@ -7,7 +7,7 @@
     'description': "",
     'author': "Alitec Pte Ltd",
     'website': "http://www.alitec.sg",
-    'version': '0.1.0.0.7',
+    'version': '0.1.0.0.8',
     'depends': ['crm', 'website_crm', 'website', 'business_appointment_website', 'portal'],
     'data': [
         'views/contact_us.xml',
